e619530781cbba46d6aadf30585e04d9bdeee49e
Michelle Miller <chellimiller@users.noreply.github.com>
2017-05-02 12:10:00 -0500

    Update ls.bat
    Meets minimum requirements. Needs improvements to be full working ls command

--- ls.bat
+++ ls.bat
@@ -7,24 +7,15 @@ setlocal
 
 rem Add a blank line and go to getArgs
 echo.
+set option=/O:G
 goto getArgs
 
-:recursive
+rem Add recursive option
+:r
 echo Options: -R
-goto next
-
-:nonrecursive
-goto next
-
-rem Summarize directory information
-:summarize
-echo Directory Summary -
-echo       Total files:        %numF%
-echo       Total directories:  %numD%
-echo       Total links:        %numL%
-echo       Total other:        %numO%
-echo       Total entries:      %numE%
-goto end
+set option=%option% /S
+echo.
+goto dir
 
 rem Check arguments and set input directory
 :getArgs
@@ -36,21 +27,16 @@ if [%1]==[] (
 	) else (
         set input=%2
 	)
-	goto recursive
+	goto r
 ) else (
     set input=%1
-	goto nonrecursive
 )
 
-:next
-echo Directory: %input%
-
-
-:end
-
+rem Use dir command
+:dir
+echo.
+dir "%input%" %option%
 rem End all local variables created in the script
 endlocal
-
 rem Echo a blank line, then pause and wait for user
 echo.
-pause
